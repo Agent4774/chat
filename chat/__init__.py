567cd83d1e480b37f1b27c55b7ef366e1891ef3f
Sasha <successamaz@gmail.com>
2021-05-29 12:52:18 +0300

    Chat

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '95e1453ba47ad25bcfb83b181a587a55')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db')
 db = SQLAlchemy(app)
+CORS(app)
 socketio = SocketIO(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'sign_in'
